36d039d9928215e4db097cf66e43f4674e2af5ad
Abrham Tadedse <atadesse976@gmail.com>
2026-08-04 11:01:25 +0300

    buiding a notification redux

--- src/features/notifications/notificationSlice.js
+++ src/features/notifications/notificationSlice.js
@@ -6,6 +6,7 @@ import { fetchNotifications,readAllNotif,readOneNotification } from "../../servi
 
 const initialState = {
     notifications : [],
+    unreadCount  :0,
     loading : false,
     error : null,
 }
@@ -33,7 +34,7 @@ export const readAll = createAsyncThunk("/readAllNotifications", async(_, {rejec
 
 export const readOne = createAsyncThunk("/readSingleNotification", async({notId}, {rejectWithValue})=>{
     try { 
-        const response = await readOne(notId);
+        const response = await readOneNotification(notId);
         return response.data;
         
     } catch (e) {
@@ -50,6 +51,71 @@ const notificationSlice = createSlice({
     name : notificationSlice,
     initialState,
     reducers : {
-        
+        clearNotificationError : ((state)=>{
+            state.error = null
+        }),
+        clearNotificationMessage : ((state)=>{
+            state.message = null
+        }),
+        resetNotitfications : ((state)=>{
+            state.notifications = [],
+            state.error = null,
+            state.loading = false,
+            state.message = null
+        })
+    },
+    extraReducers : (builder)=>{
+        builder.addCase(fetchAllNotif.pending, (state)=>{
+            state.loading = true,
+            state.error = null
+        })
+        .addCase(fetchAllNotif.fulfilled, (state, action)=>{
+            state.loading = false,
+            state.notifications = action.payload,
+             state.unreadCount = action.payload.filter((notification)=> 
+             !(notification.isRead)).length
+        })
+        .addCase(fetchAllNotif.rejected, (state, action)=>{
+            state.loading = false,
+            state.error = action.payload
+        })
+        .addCase(readOne.fulfilled, (state,action)=>{
+
+            const notification = state.notifications.find((n)=>
+                n._id === action.payload._id
+            );
+            if(notification){
+                notification._isRead = true
+            }
+            state.unreadCount--;
+
+        })
+        .addCase(readOne.rejected, (state, action)=>{
+            state.error = action.payload
+        })
+        .addCase(readAll.fulfilled, (state)=>{
+              state.notifications.forEach((notification) => {
+              notification.isRead = true;
+          });
+            state.loading = false,
+            state.unreadCount = 0
+        })
+        .addCase(readAll.rejected, (state, action)=> {
+            state.loading = false,
+            state.error = action.payload
+        }) 
+
+
+
+
     }
 })
+
+
+export const {
+    clearNotificationError,
+    clearNotificationMessage,
+    resetNotitfications
+} = notificationSlice.actions;
+
+export default notificationSlice.reducer;
